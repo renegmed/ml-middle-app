00c11ee583ca2fc53509bd376037df04f7c12e3d
ren medal <rmedal@pop-os.localdomain>
2020-03-12 13:43:02 -0400

    added py client to call aws lambda chalice.

--- fashion-mnist/project/app.py
+++ fashion-mnist/project/app.py
@@ -1,7 +1,7 @@
 from chalice import Chalice
 from chalice import BadRequestError
 
-import base64, cv2, os 
+import base64, cv2, os, sys
 #import boto3
 import json 
 import numpy as np
@@ -60,44 +60,57 @@ def index():
    
     imagearr = imagearr.reshape(-1, 28, 28, 1) 
 
-    data = json.dumps({"signature_name": "serving_default", "instances": imagearr }, cls=NumpyArrayEncoder)
-    #print(data)
+    # print(imagearr)
+ 
+    # data = json.dumps({"signature_name": "serving_default", "instances": imagearr }, cls=NumpyArrayEncoder)
 
-    headers = {"content-type": "application/json"} 
-    r = requests.post('http://localhost:8080/invocations', data=data, headers=headers)
-    j = r.json()
-    #print(j.keys())
-    #print(j)
+    # return {'response': data }
 
-    # It looks like a 2-D array, let's check its shape
-    pred = np.array(j['predictions'])
+
+
+    # data = json.dumps({"signature_name": "serving_default", "instances": imagearr }, cls=NumpyArrayEncoder)
+    # #print(data)
+
+    # headers = {"content-type": "application/json"} 
+    # r = requests.post('http://localhost:8080/invocations', data=data, headers=headers)
+    # j = r.json()
+    # #print(j.keys())
+    # #print(j)
+
+    # # It looks like a 2-D array, let's check its shape
+    # pred = np.array(j['predictions'])
      
-    # This is the N x K output array from the model
-    # pred[n,k] is the probability that we believe the nth sample belongs to the kth class
+    # # This is the N x K output array from the model
+    # # pred[n,k] is the probability that we believe the nth sample belongs to the kth class
 
-    # Get the predicted classes
-    pred = pred.argmax(axis=1)
+    # # Get the predicted classes
+    # pred = pred.argmax(axis=1)
 
-    # Map them back to strings
-    pred = [labels[i] for i in pred]
-    print("Predicted label: {}".format(pred)) 
+    # # Map them back to strings
+    # pred = [labels[i] for i in pred]
+    # print("Predicted label: {}".format(pred)) 
 
-    return {'response': pred}
+    # return {'response': pred}
 
 
-    # if 'ENDPOINT_NAME' not in os.environ:
-    #     raise BadRequestError('Missing endpoint')
+
+
+    if 'ENDPOINT_NAME' not in os.environ:
+        return {'response': 'Missing endpoint'}
+        #raise BadRequestError('Missing endpoint')
     
-    # endpoint = os.environ['ENDPOINT_NAME']
-    # print("++++ endpoint:\n{}".format(endpoint))
+    endpoint = os.environ['ENDPOINT_NAME']
+    print("++++ endpoint:\n{}".format(endpoint))
  
-    # runtime = boto3.Session().client(service_name='sagemaker-runtime', region_name='us-east-1')
-    # response = runtime.invoke_endpoint(EndpointName=endpoint, ContentType='application/x-image', Body=imagearr)
-    # resp = response['Body'].read().decode()  # byte array
+    try:
+        runtime = boto3.Session().client(service_name='sagemaker-runtime', region_name='us-east-1')
+        response = runtime.invoke_endpoint(EndpointName=endpoint, ContentType='application/javascript', Body=imagearr)
+        resp = response['Body'].read().decode()  # byte array
+        print("+++++ response:\n{}".format(resp))
+ 
+        return {'response': resp}
+    except:
 
-    # print("+++++ response:\n{}".format(resp))
- 
-    # return {'response': resp}
-  
+        return {'response': sys.exc_info()[0]} 
 
     
---
+++ fashion-mnist/project/post_aws.sh
@@ -0,0 +1,12 @@
+
+export URL='https://hzu27pbk74.execute-api.us-east-1.amazonaws.com/api/'
+export PIC='images/meters2.jpeg'  # working
+export PIC_1='images/24.jpg'    # working
+export PIC_2='images/57.jpg'    # working
+export PIC_3='images/bubble_red_dress.jpeg'  # working
+export PIC_4='images/black_ankle_boot.jpg' # working
+
+
+#(echo '{"data": "'; base64 $PIC; echo '", "height": 28 , "width": 28}') | curl -X POST -H "Content-Type: application/json" -d @-  $URL
+
+curl -X POST -H 'Accept: application/json' -H 'Content-Type: image/jpg' --data-binary @$PIC_2 $URL  
